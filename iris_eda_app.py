6963bb6f4227de9cb3843b4de40aed8e5e4ca9c5
shijie.wang2 <shijie.wang2@amh-group.com>
2022-08-07 17:51:39 +0800

    first mod

--- iris_eda_app.py
+++ iris_eda_app.py
@@ -4,5 +4,11 @@ def main():
 	"""A simple EDA App"""
 	st.title("Iris EDA APP with Streamlit")
 	st.subheader("ST app test")
+
+	activity = ["eda","prediction","countries","metrics","about"]
+	choice = st.sidebar.selectbox("Choose Activity",activity)
+
+	if activity == 'eda':
+		st.subheader("EDA")
 if __name__ == '__main__':
 	main()
