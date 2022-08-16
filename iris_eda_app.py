5244c1fa68c72ca1330df50d183e4e23b5fec6f4
shijie.wang2 <shijie.wang2@amh-group.com>
2022-08-16 23:39:08 +0800

    modify subheader

--- iris_eda_app.py
+++ iris_eda_app.py
@@ -3,12 +3,16 @@ import streamlit as st
 def main():
 	"""A simple EDA App"""
 	st.title("Iris EDA APP with Streamlit")
-	st.subheader("ST app test")
+	# st.subheader("ST app test")
 
 	activity = ["eda","prediction","countries","metrics","about"]
 	choice = st.sidebar.selectbox("Choose Activity",activity)
 
 	if activity == 'eda':
 		st.subheader("EDA")
+		st.text("EDA Part")
+	if activity == 'prediction':
+		st.subheader("Prediction")
+		st.text("Pred Part")
 if __name__ == '__main__':
 	main()
